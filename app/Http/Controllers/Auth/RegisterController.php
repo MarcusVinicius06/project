6e2462e91affa9a60d5944fa31973559ba913722
Marcus Vinicius <marcusviniciusdasilva6@gmail.com>
2019-09-28 12:10:03 -0300

    user admin

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -68,7 +68,7 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
-        $user->assignRole('administrador');
+        $user->assignRole('admin');
         return $user;
     }
 }
